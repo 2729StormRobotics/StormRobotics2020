29cf50e40ba9e798e3f6db4d243fa105932621d0
Jeremy Sher <44417289+jeremysher13@users.noreply.github.com>
2020-02-03 17:27:02 -0500

    added motors and encoders

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -7,17 +7,37 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.LauncherConstants;
 
 public class Launcher extends SubsystemBase {
+
+  private final CANSparkMax m_leftLauncher;
+  private final CANSparkMax m_rightLauncher;
+
+  private final CANEncoder m_leftEncoder;
+  private final CANEncoder m_rightEncoder;
+
   /**
    * Creates a new Launcher.
    */
   public Launcher() {
+    // Instantiate the motors.
+    m_leftLauncher = new CANSparkMax(LauncherConstants.kLeftLauncherMotorPort, MotorType.kBrushless);
+    m_rightLauncher = new CANSparkMax(LauncherConstants.kRightLauncherMotorPort, MotorType.kBrushless);
 
+    // Instantiate the encoder.
+    m_leftEncoder = m_leftLauncher.getEncoder();
+    m_rightEncoder = m_rightLauncher.getEncoder();
+
+    // Initialize the the motors.
+    motorInit(m_leftLauncher, false);
+    motorInit(m_rightLauncher, true);
   }
 
   /**
@@ -30,6 +50,11 @@ public class Launcher extends SubsystemBase {
     motor.restoreFactoryDefaults(); // Just in case any settings persist between reboots.
     motor.setIdleMode(IdleMode.kCoast); // Set the motor to coast mode, so that we don't lose momentum when we stop shooting.
     motor.setInverted(invert); // Whether or not to invert motor.
+    encoderInit(motor.getEncoder());
+  }
+
+  private void encoderInit(CANEncoder encoder) {
+    
   }
 
   @Override
