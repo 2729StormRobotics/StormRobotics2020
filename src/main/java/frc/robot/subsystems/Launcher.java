2c03137d007be7b2cffdb3553bbca9c718983d41
Varun <varun.s1118@gmail.com>
2020-02-04 16:10:48 -0500

    Added Speeds and Logs
    Left/right/average motor speeds and displayed to dashboard

--- src/main/java/frc/robot/subsystems/Launcher.java
+++ src/main/java/frc/robot/subsystems/Launcher.java
@@ -4,25 +4,26 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+ 
 package frc.robot.subsystems;
-
+ 
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.LauncherConstants;
-
+ 
 public class Launcher extends SubsystemBase {
-
+ 
   private final CANSparkMax m_leftLauncher;
   private final CANSparkMax m_rightLauncher;
-
+ 
   private final CANEncoder m_leftEncoder;
   private final CANEncoder m_rightEncoder;
-
+ 
   /**
    * Creates a new Launcher.
    */
@@ -30,16 +31,16 @@ public class Launcher extends SubsystemBase {
     // Instantiate the motors.
     m_leftLauncher = new CANSparkMax(LauncherConstants.kLeftLauncherMotorPort, MotorType.kBrushless);
     m_rightLauncher = new CANSparkMax(LauncherConstants.kRightLauncherMotorPort, MotorType.kBrushless);
-
+ 
     // Instantiate the encoder.
     m_leftEncoder = m_leftLauncher.getEncoder();
     m_rightEncoder = m_rightLauncher.getEncoder();
-
+ 
     // Initialize the the motors.
     motorInit(m_leftLauncher, false);
     motorInit(m_rightLauncher, true);
   }
-
+ 
   /**
    * Restore factory defaults, set idle mode to coast, and invert motor.
    * 
@@ -52,7 +53,7 @@ public class Launcher extends SubsystemBase {
     motor.setInverted(invert); // Whether or not to invert motor.
     encoderInit(motor.getEncoder());
   }
-
+ 
   /**
    * Initialize an encoder.
    * 
@@ -64,7 +65,7 @@ public class Launcher extends SubsystemBase {
     encoder.setVelocityConversionFactor(LauncherConstants.kEncoderSpeedPerPulse);
     encoderReset(encoder); // Reset the encoder to 0, just in case
   }
-
+ 
   /**
    * Reset an encoder's position.
    * 
@@ -73,7 +74,38 @@ public class Launcher extends SubsystemBase {
   private void encoderReset(CANEncoder encoder) {
     encoder.setPosition(0);
   }
-
+ 
+  //sets speed of launcher motors to 0
+  private void stopMotors(CANSparkMax leftLauncher, CANSparkMax rightLauncher) {
+    leftLauncher.set(0);
+    rightLauncher.set(0);
+  }
+ 
+  //get left speed in RPM
+  private double getLeftSpeed() {
+    return m_leftEncoder.getVelocity();
+  }
+ 
+  //get right speed in RPM
+  private double getRightSpeed() {
+    return m_rightEncoder.getVelocity();
+  }
+ 
+  //get average speed
+  private double getSpeed() {
+    return ((getLeftSpeed() + getRightSpeed()) / 2.0);
+  }
+ 
+  //add info to the dashboard
+  public void log() {
+    SmartDashboard.putNumber("Left Launcher Speed(RPM)", m_leftEncoder.getVelocity());
+    SmartDashboard.putNumber("Right Launcher Speed(RPM)", m_rightEncoder.getVelocity());
+    SmartDashboard.putNumber("Average Launcher Speed(RPM)", getSpeed());
+  }
+ 
+  
+ 
+ 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
