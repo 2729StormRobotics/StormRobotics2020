ebc9d1e4457d188d7b78ef2720528924d2d150e2
1030shruti <30726705+1030shruti@users.noreply.github.com>
2020-02-04 17:13:16 -0500

    Create Hopper.java

---
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.LoadingConstants;
+
+public class Hopper extends SubsystemBase {
+
+  private final TalonSRX m_hopperMotor;
+
+  /**
+   * Creates a new Hopper.
+   */
+  public Hopper() {
+    m_hopperMotor = new TalonSRX(LoadingConstants.kLoadingMotorPort);
+  
+    motorInit(m_hopperMotor);
+    
+  }
+
+  public void motorInit(TalonSRX motor) {
+    motor.configFactoryDefault(); // configures the factory defaults in case and changes were previously made to the motor  
+  }
+
+  public void startHopperMotor(double speed) {
+    m_hopperMotor.set(ControlMode.Current, speed);
+  }
+
+  public void stopHopperMotor() {
+    m_hopperMotor.set(ControlMode.PercentOutput, 0);
+
+  }
+
+  public void log() {
+    SmartDashboard.putNumber("Hopper Motor Speed", m_hopperMotor.getActiveTrajectoryVelocity());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
