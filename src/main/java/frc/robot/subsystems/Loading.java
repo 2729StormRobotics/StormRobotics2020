696468bf37956abffcfdacf09cce364730c00cfb
irinale629823 <57159202+irinale629823@users.noreply.github.com>
2020-02-06 16:41:28 -0500

    set inverted and current limit values

--- src/main/java/frc/robot/subsystems/Loading.java
+++ src/main/java/frc/robot/subsystems/Loading.java
@@ -28,39 +28,42 @@ public class Loading extends SubsystemBase {
 
   public Loading() {
 
+    
     m_holderMotor = new CANSparkMax(LoadingConstants.kLowCellevatorMotorPort, MotorType.kBrushed);
     m_loaderMotor = new CANSparkMax(LoadingConstants.kHighCellevatorMotor2Port, MotorType.kBrushed);
     m_beamBreakHolder = new DigitalInput(LoadingConstants.kBeamBreakOutput1Port);
     m_beamBreakLoader = new DigitalInput(LoadingConstants.kBeamBreakOutput2Port);
 
     //intializes the motors 
-    motorInit(m_loaderMotor);
-    motorInit(m_holderMotor);
+    motorInit(m_holderMotor, LoadingConstants.kHolderMotorInverted);
+    motorInit(m_loaderMotor, LoadingConstants.kLoaderMotorInverted);
     
   }
 
-  private void motorInit(CANSparkMax motor) {
+  private void motorInit(CANSparkMax motor, boolean invert) {
     motor.restoreFactoryDefaults(); // Reset settings in motor in case they are changed
     motor.setIdleMode(IdleMode.kBrake); // Sets the motors to brake mode from the beginning
+    motor.setInverted(invert);
+    motor.setSmartCurrentLimit(LoadingConstants.kLoadingCurrentLimit);
   }
 
   // starts the loading motors
-  public void startLoaderMotor (double speed) {
+  public void runLoaderMotor (double speed) {
     m_loaderMotor.set(speed);
   }
 
-  public void startHolderMotor (double speed) {
+  public void runHolderMotor (double speed) {
     m_holderMotor.set(speed);
   }
 
   // gets the beam break value to see if there is a power cell present at the top of the cellavator
-  public void getHolderBeamBreakValue () {
+  public void isBallHeld() {
     m_beamBreakHolder.get();
     }
 
   // gets the beam break value to see if there is a power cell present at bottom at the cellavator where the loader motor is
 
-  public void getLoaderBeamBreakValue() {
+  public void isBallLoaded() {
     m_beamBreakLoader.get();
   }
 
@@ -72,18 +75,18 @@ public class Loading extends SubsystemBase {
 
   // only stops the loader motor
   public void stopLoaderMotor() {
-    m_loaderMotor.set(0);
+    runLoaderMotor(0);
   }
 
   // only stops the holder motor
   public void stopHolderMotor() {
-    m_holderMotor.set(0);
+    runHolderMotor(0);
   }
 
   // displays data onto SmartDashboard
   public void log() {
     SmartDashboard.putData("Holder Beam Value", m_beamBreakHolder);
-    SmartDashboard.putData("Loader Bean Value", m_beamBreakLoader);
+    SmartDashboard.putData("Loader Beam Value", m_beamBreakLoader);
     SmartDashboard.putNumber("Holder Motor Speed", m_holderMotor.get());
     SmartDashboard.putNumber("Loader Motor Speed", m_loaderMotor.get());
   }
