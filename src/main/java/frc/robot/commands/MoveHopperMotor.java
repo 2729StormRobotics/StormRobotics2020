30b18c1220f310cae36972a5333540e1df6ab627
AlexaZoubrilov <57159212+AlexaZoubrilov@users.noreply.github.com>
2020-02-08 14:01:41 -0500

    Updated MoveHopperMotor

--- src/main/java/frc/robot/commands/MoveHopperMotor.java
+++ src/main/java/frc/robot/commands/MoveHopperMotor.java
@@ -9,6 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Hopper;
+import frc.robot.Constants.HopperConstants;
 
 public class MoveHopperMotor extends CommandBase {
   private final Hopper m_Hopper;
@@ -31,7 +32,7 @@ public class MoveHopperMotor extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Hopper.startHopperMotor(kHopperMotorSpeed);
+    m_Hopper.startHopperMotor(HopperConstants.kHopperMotorSpeed);
   }
 
   // Called once the command ends or is interrupted.
