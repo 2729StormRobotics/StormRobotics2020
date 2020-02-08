65df0ad70543a548fe3f648e4c7b385b06d09dc1
Clam <57159181+133622@users.noreply.github.com>
2020-02-08 10:16:06 -0500

    Create LimelightAlign.java

---
+++ src/main/java/frc/robot/commands/LimelightAlign.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+
+import frc.robot.subsystems.*;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class LimelightAlign extends PIDCommand {
+  // Adds variables for the limelight and drivetrain subsystem
+  private final Limelight m_limelight;
+  private final Drivetrain m_drivetrain;
+
+  /**
+   * Creates a new LimelightAlign.
+   */
+  public LimelightAlign(Limelight limesub, Drivetrain drivesub) {
+    super(
+        // The controller that the command will use
+        new PIDController(0, 0, 0),
+        // This should return the measurement
+        () -> 0,
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> {
+          // Use the output here
+        });
+        
+        m_limelight = limesub;
+        m_drivetrain = drivesub;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
