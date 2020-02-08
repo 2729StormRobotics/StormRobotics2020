ba41974958ce75091ed9f60c7b09d8f1b52d2aa9
Robotics <robotics@LENPLTWZXB.lr.k12.nj.us>
2020-02-08 09:11:20 -0500

    Update Celevator.java

--- src/main/java/frc/robot/subsystems/Celevator.java
+++ src/main/java/frc/robot/subsystems/Celevator.java
@@ -14,7 +14,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.CelevatorConstants;;
+import frc.robot.Constants.CelevatorConstants;
 
 public class Celevator extends SubsystemBase {
   /**
