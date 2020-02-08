d993cb3e8cf1955789a49f8beee25692ee2c94e0
Josh Wright <57159225+653522@users.noreply.github.com>
2020-02-08 13:53:31 -0500

    Finish ManualDrive Command

--- src/main/java/frc/robot/commands/ManualDrive.java
+++ src/main/java/frc/robot/commands/ManualDrive.java
@@ -7,24 +7,36 @@
 
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 public class ManualDrive extends CommandBase {
-  
   private final Drivetrain m_drivetrain;
-  private final double m_leftSpeed;
-  private final double m_rightSpeed;
+  private final DoubleSupplier m_reverseSpeed;
+  private final DoubleSupplier m_forwardSpeed;
+  private final DoubleSupplier m_turnSpeed;
 
-  public ManualDrive(double left, double right, Drivetrain drivetrain) {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-    m_drivetrain = drivetrain;
-    m_leftSpeed = left;
-    m_rightSpeed = right;
+  /**
+   * @param reverse     The backwards speed of the robot
+   * @param forward     The forwards speed of the robot
+   * @param turn        The turning speed of the robot
+   * @param subsystem   The subsystem being used by the command--in this case, the drivetrain
+   */
+
+  public ManualDrive(DoubleSupplier reverse, DoubleSupplier forward, DoubleSupplier turn, Drivetrain subsystem) {
+    // These are the local instances of the parameter variables for ManualDrive.
+    m_drivetrain = subsystem;
+    m_reverseSpeed = reverse;
+    m_forwardSpeed = forward;
+    m_turnSpeed = turn;
+
+    // Attach the local instane of the subsystem--in this case, drivetrain--to the command.
+    addRequirements(m_drivetrain);
   }
 
-  // Called just before this Command runs the first time
+  // Called just before this Command runs the first time.
   @Override
   public void initialize() {
   }
@@ -32,24 +44,30 @@ public class ManualDrive extends CommandBase {
   // Called repeatedly when this Command is scheduled to run
   @Override
   public void execute() {
-    m_drivetrain.tankDrive(m_leftSpeed, m_rightSpeed, true);
+    // Repeatedly passes the parameters--reverse speed, forward speed, turn speed,
+    // and if inputs are squared, which increases fine control at lower speeds--
+    //to the triggerDrive
+    m_drivetrain.triggerDrive(m_reverseSpeed.getAsDouble(), m_forwardSpeed.getAsDouble(), m_turnSpeed.getAsDouble(), true);
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   public boolean isFinished() {
+    // Since ManualDrive is the default, it should never stop running
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
   public void end(boolean interrupted) {
+    // Stop the robot if this command ends naturally or is interrupted.
+    // It will not end naturally.
     m_drivetrain.stopDrive();
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  public void interrupted() {
-  }
+  // // Called when another command which requires one or more of the same
+  // // subsystems is scheduled to run
+  // @Override
+  // public void interrupted() {
+  // }
 }
