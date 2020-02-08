8a15b33f853d44297848752ede0676a1653d2bac
Jeremy Sher <44417289+jeremysher13@users.noreply.github.com>
2020-02-08 13:30:12 -0500

    completed PointTurn command

--- src/main/java/frc/robot/commands/PointTurn.java
+++ src/main/java/frc/robot/commands/PointTurn.java
@@ -9,30 +9,59 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.Drivetrain;
+
+import static frc.robot.Constants.DriveConstants.*;
 
 public class PointTurn extends PIDCommand {
+
+  private final Drivetrain m_drivetrain;
+
+  private static double m_initialAngle;
+
   /**
-   * Creates a new PointTurn.
+   * Rotates the robot a certain amount of degrees using PID.
+   * 
+   * @param angle The angle in degrees by which the robot will rotate. Must be between -180 and 180 degrees.
+   * @param drivetrain Pass in the drivetrain for the command to use.
+   * 
    */
-  public PointTurn() {
+  public PointTurn(double angle, Drivetrain drivetrain) {
     super(
         // The controller that the command will use
-        new PIDController(0, 0, 0),
-        // This should return the measurement
-        () -> 0,
-        // This should return the setpoint (can also be a constant)
-        () -> 0,
-        // This uses the output
+        new PIDController(PointTurnPID.kP, PointTurnPID.kI, PointTurnPID.kD),
+        // This returns the measurement (current angle of the robot relative to initial angle)
+        () -> drivetrain.getRobotAngle() - m_initialAngle,
+        // This returns the setpoint (target angle relative to initial angle)
+        () -> angle,
+        // This uses the output (rotates the robot using arcadeDrive)
         output -> {
-          // Use the output here
+          // Note: uses negative output because arcadeDrive considers clockwise angles to be positive,
+          // but the gyro considers counter-clockwise angles to be positive.
+          drivetrain.arcadeDrive(0, -output, false);
         });
-    // Use addRequirements() here to declare subsystem dependencies.
-    // Configure additional PID options by calling `getController` here.
+    // Assign drivetrain member variable to drivetrain passed in constructor.
+    m_drivetrain = drivetrain;
+    // Add Drivetrain as a required subsystem.
+    addRequirements(drivetrain);
+    // Set the position and velocity tolerances.
+    getController().setTolerance(PointTurnPID.kPositionTolerance, PointTurnPID.kVelocityTolerance);
+
+    // Set the wrap-around point for the PIDController to move in the shortest route to the setpoint.
+    // Note: this clamps the setpoint between -180 and 180, meaning that the setpoint should never be outside that range.
+    getController().enableContinuousInput(-180, 180);
+  }
+
+  // Runs when the command is scheduled.
+  @Override
+  public void initialize() {
+    // Save the angle of the gyro at the beginning of PointTurn.
+    m_initialAngle = m_drivetrain.getRobotAngle();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return getController().atSetpoint();
   }
 }
