b178141ebe34b73746ec59a606c0d5a27e9036a0
Josh Wright <57159225+653522@users.noreply.github.com>
2020-02-08 15:16:44 -0500

    Update Command to the new style

--- src/main/java/frc/robot/commands/DriveManually.java
+++ src/main/java/frc/robot/commands/DriveManually.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,17 +10,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
-public class ManualDrive extends CommandBase {
+public class DriveManually extends CommandBase {
   private final Drivetrain m_drivetrain;
-  private final Double m_leftSpeed;
-  private final Double m_rightSpeed;
+  private final double m_leftSpeed;
+  private final double m_rightSpeed;
 
 /**
  * @param leftSpeed   speed of left motors
  * @param rightSpeed  speed of right motors
  * @param subsystem   the subsystem being used--in this case, the drivetrain
  */
-  public ManualDrive(Double leftSpeed, Double rightSpeed, Drivetrain subsystem) {
+  public DriveManually(double leftSpeed, double rightSpeed, Drivetrain subsystem) {
     // These are the local instances of the parameter variables for ManualDrive.
     m_drivetrain = subsystem;
     m_leftSpeed = leftSpeed;
@@ -35,7 +35,7 @@ public class ManualDrive extends CommandBase {
   public void initialize() {
   }
 
-  // Called repeatedly when this Command is scheduled to run
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     // Repeatedly passes the parameters--left speed, right speed, and if inputs are
@@ -43,14 +43,7 @@ public class ManualDrive extends CommandBase {
     m_drivetrain.tankDrive(m_leftSpeed, m_rightSpeed, true);
   }
 
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  public boolean isFinished() {
-    // Since ManualDrive is the default, it should never stop running
-    return false;
-  }
-
-  // Called once after isFinished returns true
+  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     // Stop the robot if this command ends naturally or is interrupted.
@@ -58,9 +51,10 @@ public class ManualDrive extends CommandBase {
     m_drivetrain.stopDrive();
   }
 
-  // // Called when another command which requires one or more of the same
-  // // subsystems is scheduled to run
-  // @Override
-  // public void interrupted() {
-  // }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+     // Since ManualDrive is the default, it should never stop running
+     return false;
+  }
 }
