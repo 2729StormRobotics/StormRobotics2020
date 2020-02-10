6b1fc1706de8a32cfd5713b31cdc46774c7eb4f6
AlexaZoubrilov <57159212+AlexaZoubrilov@users.noreply.github.com>
2020-02-10 17:30:07 -0500

    Created FixedLaunch command

--- src/main/java/frc/robot/commands/FixedLaunch.java
+++ src/main/java/frc/robot/commands/FixedLaunch.java
@@ -7,22 +7,23 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
+import com.revrobotics.ControlType;
+
+import com.revrobotics.CANPIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Launcher;
+import jdk.vm.ci.meta.Constant;
+import frc.robot.Constants;
 import frc.robot.Constants.LauncherConstants;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 public class FixedLaunch extends CommandBase {
-  private final double kP;
-  private final double kI;
-  private final double Kd;
-  private final double kF;
-  
+
   private final double m_launchSpeed;
   private final Launcher m_Launcher;
+
   /**
    * Creates a new FixedLaunch.
    */
@@ -35,19 +36,18 @@ public class FixedLaunch extends CommandBase {
   // Called just before this Command runs the first time.
   @Override
   public void initialize() {
-    
+    m_Launcher.revToSpeed(m_launchSpeed);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
+    m_Launcher.stopRevving();
   }
 
   // Returns true when the command should end.
