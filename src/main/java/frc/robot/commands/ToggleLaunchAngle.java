8be73679b60c5f75eaf492f0dd4136868b087419
113020 <32312357+113020@users.noreply.github.com>
2020-02-10 17:32:23 -0500

    Revert "Update ToggleLaunchAngle.java"
    This reverts commit 5ddecd63a8289dcfd9493cf1ffe9818685fc4c8c.

--- src/main/java/frc/robot/commands/ToggleLaunchAngle.java
+++ src/main/java/frc/robot/commands/ToggleLaunchAngle.java
@@ -12,6 +12,7 @@ import frc.robot.subsystems.Launcher;
 
 public class ToggleLaunchAngle extends CommandBase {
   private final Launcher m_launchAngle;
+  private final boolean isExtended;
   
   /**
    * Creates a new ToggleLaunchAngle.
