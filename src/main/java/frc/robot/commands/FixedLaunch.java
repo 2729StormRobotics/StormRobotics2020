742c0d34a174d9c8eada9848a8b23fa4f8ffe136
AlexaZoubrilov <57159212+AlexaZoubrilov@users.noreply.github.com>
2020-02-10 16:43:29 -0500

    Updated FixedLaunch

--- src/main/java/frc/robot/commands/FixedLaunch.java
+++ src/main/java/frc/robot/commands/FixedLaunch.java
@@ -7,39 +7,52 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Launcher;
 import frc.robot.Constants.LauncherConstants;
 
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 public class FixedLaunch extends CommandBase {
-  Launcher m_Launcher;
+  private final double kP;
+  private final double kI;
+  private final double Kd;
+  private final double kF;
+  
+  private final double m_launchSpeed;
+  private final Launcher m_Launcher;
   /**
    * Creates a new FixedLaunch.
    */
-  public FixedLaunch(Launcher subsystems) {
-    m_Launcher = subsystems;
+  public FixedLaunch(double launchSpeed, Launcher subsystem) {
+    m_launchSpeed = launchSpeed;
+    m_Launcher = subsystem;
     addRequirements(m_Launcher);
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  // Called when the command is initially scheduled.
+  // Called just before this Command runs the first time.
   @Override
   public void initialize() {
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+     return false;
   }
 }
