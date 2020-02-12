6cfa791a7a6bdc1fd0159c6fb60d1b5601e8eaf1
Josh Wright <57159225+653522@users.noreply.github.com>
2020-02-12 16:34:55 -0500

    Initialize subsystems and send subsystem data to SmartDashboard on RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,18 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import static edu.wpi.first.wpilibj.GenericHID.Hand;
+import static edu.wpi.first.wpilibj.XboxController.Button.*;
+import static frc.robot.Constants.*;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -19,13 +28,38 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private final Cellevator m_cellevator = new Cellevator();
+  private final CellevatorLoader m_cellevatorLoader = new CellevatorLoader();
+  private final Climbers m_climbers = new Climbers();
+  private final ControlPanel m_controlPanel = new ControlPanel();
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final Hopper m_hopper = new Hopper();
+  private final Intake m_intake = new Intake();
+  private final Launcher m_launcher = new Launcher();
+  private final Limelight m_limelight = new Limelight();
+  private final Party m_party = new Party();
 
-
+  private final XboxController m_driverController = new XboxController(kDriverControlPort);
+  private final XboxController m_weaponsController = new XboxController(kWeaponsControlPort);
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    // Set default commands once button bindings are finalized
+    SmartDashboard.putData("Cellevator Subsystem", m_cellevator);
+    SmartDashboard.putData("Cellevator Loader Subsystem", m_cellevatorLoader);
+    SmartDashboard.putData("Climbers Subsystem", m_climbers);
+    SmartDashboard.putData("Control Panel Subsystem", m_controlPanel);
+    SmartDashboard.putData("Drivetrain Subsystem", m_drivetrain);
+    SmartDashboard.putData("Hopper Subsystem", m_hopper);
+    SmartDashboard.putData("Intake Subsystem", m_intake);
+    SmartDashboard.putData("Launcher Subsystem", m_launcher);
+    SmartDashboard.putData("Limelight Subsystem", m_limelight);
+    SmartDashboard.putData("Party Subsystem", m_party);
+
+    // Show what command each subsystem is running on the SmartDashboard
+
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -37,8 +71,21 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    // Set button bindings here once they are decided upon and finalized.
   }
 
+public void updateDashboard() {
+  m_cellevator.log();
+  m_cellevatorLoader.log();
+  m_climbers.log();
+  // Control Panel log does not exist, but it may not be necessary to have.
+  m_drivetrain.log();
+  m_hopper.log();
+  m_intake.log();
+  m_launcher.log();
+  m_limelight.log();
+  // Party log does not exist, but it may not be necessary to have.
+}
 
   //commented out because no auto yet
   /**
