3e36ae16e7c6d10758670587b87ff709971f499e
113020 <32312357+113020@users.noreply.github.com>
2020-02-13 15:55:27 -0500

    Created LaunchAngle Instant Command

--- src/main/java/frc/robot/commands/LaunchAngle.java
+++ src/main/java/frc/robot/commands/LaunchAngle.java
@@ -7,17 +7,20 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Launcher;
 
-public class ToggleLaunchAngle extends CommandBase {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class LaunchAngle extends InstantCommand {
   private final Launcher m_launch;
   private final boolean m_isExtended;
   
   /**
    * Creates a new ToggleLaunchAngle.
    */
-  public ToggleLaunchAngle(Boolean extended, Launcher launcher) {
+  public LaunchAngle(Boolean extended, Launcher launcher) {
     // Set launcher subsystem equal to command parameter
     m_launch = launcher;
 
@@ -34,21 +37,5 @@ public class ToggleLaunchAngle extends CommandBase {
   @Override
   public void initialize() {
     m_launch.setLaunchPiston(m_isExtended);
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
   }
 }
