7e2a77175ae1bf45db6e01e3d83329e7caf4781b
113020 <32312357+113020@users.noreply.github.com>
2020-02-15 11:22:56 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.util.Color;
  * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
     public static final class DriveConstants {
         public static final int kLeftDriveMotor1Port = 3;
         public static final int kLeftDriveMotor2Port = 12;
@@ -199,11 +200,13 @@ public final class Constants {
         public static final int kLedBlinkinDriver2Port = 9;
 
         // Each possible LED mode correlates to a value for the Spark, as defined in the Rev Blinkin docs
-        public static final double kDancePartySparkValue = -0.97;   // Pattern: Rainbow, Pallete: Party
+        public static final double kRainbowParty = -0.97;           // Pattern: Rainbow, Pallete: Party
         public static final double kNormal = 0.53;                  // Color 1 and 2: Color Waves set as normal
         public static final double kRed = 0.61;                     // Solid Color: Red
         public static final double kOrange = 0.65;                  // Solid Color: Orange
         public static final double kGreen = 0.71;                   // Solid color: Green
+        public static final double kLime = 0.73;                    // Solid color: Lime
+        public static final double kYellow = 0.69;                  // Solid color: Yellow
     }
 
     public static final class ControlPanelConstants {
@@ -288,6 +291,7 @@ public final class Constants {
         public static final double kLimelightDistance = 50.0;
         public static final double kLimelightDistanceTolerance = 5.0;
     }
+    
     public static final class ControllerConstants {
         public static final int kDriverControlPort = 0;
         public static final int kWeaponsControlPort = 1;
