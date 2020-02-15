8bde958dd96b4457e666041340ccf015470fb4a6
Josh Wright <57159225+653522@users.noreply.github.com>
2020-02-15 14:36:10 -0500

    fixed name

--- src/main/java/frc/robot/commands/FindColor.java
+++ src/main/java/frc/robot/commands/FindColor.java
@@ -24,7 +24,7 @@ public class FindColor extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {    
-    m_findColor.setColor();
+    m_findColor.setTargetColor();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
