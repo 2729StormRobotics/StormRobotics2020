4bba8439b668b7ba17f2c571762f247e59694064
Maya Z <57159221+657323@users.noreply.github.com>
2020-02-15 10:01:53 -0500

    Update LowGearDrive.java

--- src/main/java/frc/robot/commands/LowGearDrive.java
+++ src/main/java/frc/robot/commands/LowGearDrive.java
@@ -8,17 +8,25 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Drivetrain;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 public class LowGearDrive extends InstantCommand {
-  public LowGearDrive() {
+
+  private final Drivetrain m_drivetrain;
+
+  public LowGearDrive(Drivetrain subsystem) {
+
+    m_drivetrain = subsystem;
+    addRequirements(subsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_drivetrain.shiftLow(true);
   }
 }
