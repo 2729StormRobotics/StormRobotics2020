a3c576014cc2bd93238b39c82afeffec7140d351
Varun <varun.s1118@gmail.com>
2020-02-15 15:39:12 -0500

    Created Auto

---
+++ src/main/java/frc/robot/commandgroups/Autonomous.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commandgroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import frc.robot.Constants.*;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class Autonomous extends SequentialCommandGroup {
+  /**
+   * Creates a new Autonomous.
+   */
+  public Autonomous(Drivetrain drive, Launcher launch, Intake intake, Limelight limelight) {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(
+      
+
+
+
+
+
+    );
+  }
+}
