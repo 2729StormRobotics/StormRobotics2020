a9b45517712472d8aae04cb3e185fbc9151129bf
Josh Wright <57159225+653522@users.noreply.github.com>
2020-02-15 15:34:09 -0500

    Added more comments

--- src/main/java/frc/robot/commands/MovePanel.java
+++ src/main/java/frc/robot/commands/MovePanel.java
@@ -39,6 +39,6 @@ public class MovePanel extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return (m_MovePanel.isSpun() == true && m_MovePanel.colorCount() >= 8); //ends when both condidtions are true  
   }
 }
