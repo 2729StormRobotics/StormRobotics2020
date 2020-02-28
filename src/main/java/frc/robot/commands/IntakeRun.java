3c5b9e41ddace7ddb3edef62700dc9e39440cf75
Robotics <robotics@LENPLTWZXB.lr.k12.nj.us>
2020-02-28 14:33:44 -0500

    Update IntakeRun.java

--- src/main/java/frc/robot/commands/IntakeRun.java
+++ src/main/java/frc/robot/commands/IntakeRun.java
@@ -10,8 +10,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Intake;
 
-import static frc.robot.Constants.IntakeConstants.kIntakeMotorSpeed;
-
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
