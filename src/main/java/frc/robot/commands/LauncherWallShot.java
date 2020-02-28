570a283467ceded1835ef88e1cd64d0140db92bd
Robotics <robotics@LENPLTWZXB.lr.k12.nj.us>
2020-02-28 14:33:48 -0500

    Create LauncherWallShot.java

---
+++ src/main/java/frc/robot/commands/LauncherWallShot.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Launcher;
+
+public class LauncherWallShot extends CommandBase {
+  private final Launcher m_launcher;
+
+  /**
+   * Creates a new LauncherVariableShot.
+   */
+  public LauncherWallShot(Launcher subsystem) {
+    m_launcher = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_launcher.stopLauncher();
+    m_launcher.setShortLaunchAngle();
+    m_launcher.revShortShot();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_launcher.stopLauncher();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
