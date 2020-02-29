ef4b35d3ccec951b961d4a6f575f64a0668d91f7
Robotics <robotics@LENPLTWZXB.lr.k12.nj.us>
2020-02-28 19:06:38 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,39 +7,86 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
 import frc.robot.commandgroups.*;
 import frc.robot.subsystems.*;
 
+import static frc.robot.Constants.OIConstants.*;
+
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private final Drivetrain m_drivetrain;
+  private final Vision m_vision;
+  private final Intake m_intake;
+  private final Hopper m_hopper;
+  private final Launcher m_launcher;
+  private final Climber m_climber;
+  private final Cellevator m_cellevator;
+  private final ControlPanel m_controlPanel;
+
+  private final XboxController m_driver = new XboxController(kDriverControllerPort);
+  private final XboxController m_weapons = new XboxController(kWeaponsControllerPort);
 
   /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+    CameraServer.getInstance().startAutomaticCapture();
+
+    m_drivetrain = new Drivetrain();
+    m_vision = new Vision();
+    m_intake = new Intake();
+    m_hopper = new Hopper();
+    m_launcher = new Launcher();
+    m_climber = new Climber();
+    m_cellevator = new Cellevator();
+    m_controlPanel = new ControlPanel();
+
+    m_drivetrain.setDefaultCommand(
+        new DriveManually(() -> m_driver.getY(Hand.kLeft), () -> m_driver.getY(Hand.kRight), m_drivetrain));
+
+    m_climber.setDefaultCommand(new ClimbManually(() -> m_weapons.getY(Hand.kLeft), m_climber));
+    m_cellevator.setDefaultCommand(new Cellevate(m_cellevator));
+
     // Configure the button bindings
     configureButtonBindings();
   }
 
   /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-  }
+    new JoystickButton(m_weapons, Button.kBumperRight.value).whenPressed(new IntakeAndAgitate(m_intake, m_hopper));
+    new JoystickButton(m_weapons, Button.kBumperRight.value).whenReleased(new IntakeAndAgitateStop(m_intake, m_hopper));
+    new JoystickButton(m_weapons, Button.kX.value).whenPressed(new ControlPanelSpinForRevs(m_controlPanel));
+    new JoystickButton(m_weapons, Button.kX.value).whenReleased(new ControlPanelStop(m_controlPanel));
+    new JoystickButton(m_weapons, Button.kY.value).whenPressed(new ControlPanelSpinForColor(m_controlPanel));
+    new JoystickButton(m_weapons, Button.kX.value).whenReleased(new ControlPanelStop(m_controlPanel));
+    new JoystickButton(m_weapons, Button.kA.value).whenPressed(new LauncherMode(m_launcher, m_drivetrain, m_intake, m_hopper, m_cellevator));
+    new JoystickButton(m_weapons, Button.kA.value).whenReleased(new LauncherModeStop(m_launcher, m_drivetrain, m_intake, m_hopper, m_cellevator));
+    // new JoystickButton(m_weapons, Button.kB.value).whenPressed(new IntakeToggle(m_intake));
 
+
+    // new JoystickButton(m_weapons, Button.kBumperLeft.value).whenPressed(new)
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
